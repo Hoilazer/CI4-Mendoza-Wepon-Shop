fcb3397fba34e41aa6dad97d3a3991cc26f6eb0f
Castor <castortroymendoza@gmail.com>
2025-10-09 12:51:06 +0800

    update(components): updated button, head

--- backend/app/Views/components/button.php
+++ backend/app/Views/components/button.php
@@ -1,18 +1,21 @@
 <?php
-/*function button($btnText, $btnLink, $btnType) {
-        IF ($btnType == "primary")
-            return(<button>    </button>);
 
-    }*/
+function primaryButton($btnText, $btnLink, $divClass = '')
+{
+    $btnHtml = "<button class=\"btn btn-yellow\" onclick=\"location.href='$btnLink'\">$btnText</button>";
+    return $divClass ? "<div class=\"$divClass\">$btnHtml</div>" : $btnHtml;
+}
+
+function secondaryButton($btnText, $btnLink, $divClass = '')
+{
+    $btnHtml = "<button class=\"btn btn-blue\" onclick=\"location.href='$btnLink'\">$btnText</button>";
+    return $divClass ? "<div class=\"$divClass\">$btnHtml</div>" : $btnHtml;
+}
 ?>
 
-<div class="top-right" role="navigation" aria-label="Top right">
-    <button class="btn btn-yellow" onclick="location.href='/'">Home</button>
-    <button class="btn btn-yellow" onclick="location.href='/login'">Login</button>
-    <button class="btn btn-yellow" onclick="location.href='/signup'">Signup</button>
-</div>
+<?= primaryButton('Home', '/', 'top-right') ?>
+<?= primaryButton('Login', '/login', 'top-right') ?>
+<?= primaryButton('Signup', '/signup', 'top-right') ?>
 
-<div class="bottom-right">
-    <button class="btn btn-blue" onclick="location.href='/moodboard'">Moodboard</button>
-    <button class="btn btn-blue" onclick="location.href='/roadmap'">Roadmap</button>
-</div>
+<?= secondaryButton('Moodboard', '/moodboard', 'bottom-right') ?>
+<?= secondaryButton('Roadmap', '/roadmap', 'bottom-right') ?>
